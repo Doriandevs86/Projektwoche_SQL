71d2fc56f27d32c783dcff390492503de252ed49
Ameroras <dorian.ratzmer@data-craft-students.de>
2025-01-09 19:09:15 +0100

    Load_Database
    Funktion zum laden der Daten aus den Excel-Tabellen in eine Datenbank

---
+++ load_database.py
@@ -0,0 +1,32 @@
+import sqlite3
+import pandas as pd
+
+
+def load_excel_to_database(database_name, folder_path):
+
+    conn = sqlite3.connect(database_name)
+    cursor = conn.cursor()
+
+    # Dateien, die importiert
+    excel_files = ["Mappe2.xlsx", "Musikdaten_Martin_09012025.xlsx"]
+
+    for file_name in excel_files:
+        file_path = f"{folder_path}/{file_name}"
+
+        df = pd.read_excel(file_path)
+
+        table_name = file_name.split(".")[0]
+
+        # DataFrame in die SQLite-Datenbank schreiben
+        df.to_sql(table_name, conn, if_exists="replace", index=False)
+
+        print(
+            f"Die Datei '{file_name}' wurde erfolgreich als Tabelle '{table_name}' in die Datenbank '{database_name}' importiert.")
+
+    # Verbindung schließen
+    conn.close()
+    print("Alle Daten wurden importiert und die Verbindung zur Datenbank wurde geschlossen.")
+
+
+# Die Funktion zu Testen aufrufen
+load_excel_to_database("musikdaten.db", "database")
