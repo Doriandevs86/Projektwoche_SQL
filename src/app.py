b696af903b6401b042766eea6c00100d091ee792
Ameroras <dorian.ratzmer@data-craft-students.de>
2025-01-11 05:27:43 +0100

    Dropdowns angepasst
    Dropdowns angepasst

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 import customtkinter as ctk
 from PIL import Image
-from src.util.dropdown_funtionen import get_genres_from_db
-from src.util.dropdown_funtionen import get_interpreten_from_db
-from src.util.dropdown_funtionen import get_rating_from_db
+from src.util.dropdown_funktionen import get_genres_from_db
+from src.util.dropdown_funktionen import get_interpreten_from_db
+from src.util.dropdown_funktionen import get_rating_from_db
+
+
 
 ### Hauptfenster ###
 ctk.set_appearance_mode("dark")
@@ -15,7 +17,9 @@ root.resizable(width=False, height=False)
 root.configure()
 
 
+
 ### Hintergrundbild laden ###
+
 bild_path = r"assetes/image/SL.123119.26540.04.jpg"
 bild = Image.open(bild_path)
 # CTkImage erstellen und auf Fenstergröße skalieren
@@ -23,6 +27,7 @@ hintergrundbild = ctk.CTkImage(bild, size=(800, 600))
 # CTkLabel für das Hintergrundbild erstellen
 label_hintergrund = ctk.CTkLabel(root, image=hintergrundbild, text="")
 label_hintergrund.place(relwidth=1, relheight=1)
+
 
 
 ### Buttons und Menüs ###
@@ -40,28 +45,14 @@ interpret_dropdown = ctk.CTkOptionMenu(root, variable=interpret_ver, values=inte
 interpret_dropdown.grid(column=0, row=1, padx=20, pady=10)
 
 # Rating Dropdown
-rating_ver = ctk.StringVar(value="Rating")
-rating = get_rating_from_db()
-rating_dropdown = ctk.CTkOptionMenu(root, variable=rating_ver,values=rating, bg_color="transparent")
-rating_dropdown.grid(column=0, row=2, padx=20, pady=10 )
-
-# Extras Lable
-label = ctk.CTkLabel(root, text="Extras")
-label.grid(row=0, column=1, padx=20, pady=10)
-
-# Radio-Buttons (Ja/Nein)
-radio_btn = ctk.IntVar(value=-1)
-chooice = ["Yes", "No"]
-for index, gender in enumerate(chooice):
-    ctk.CTkRadioButton(root, text=gender, value=index, variable=radio_btn).grid(row=index+1, column=1, padx=20, pady=10)
+extras_ver = ctk.StringVar(value="Extras")
+extras = ["ja", "Nein"]
+extras_dropdown = ctk.CTkOptionMenu(root, variable=extras_ver,values=extras, bg_color="transparent")
+extras_dropdown.grid(column=0, row=2, padx=20, pady=10 )
 
 # Start-Button
 start_btn = ctk.CTkButton(root, text="Start",)
 start_btn.grid(row=3, column=0, columnspan=2, padx=20, pady=10)
 
 
-
-
-
-# Hauptschleife starten
 root.mainloop()
