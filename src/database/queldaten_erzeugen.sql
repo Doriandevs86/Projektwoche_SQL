b7d206eb1a485c6dc1727ad615879a5c1e19809e
Ameroras <dorian.ratzmer@data-craft-students.de>
2025-01-11 03:27:31 +0100

    dropdown funktion geschrieben datenbank umgestellt
    habe die funktionen für die dropdownbuttons geschrieen und getestet.
    
    datenbank von sql auf postgres ugestellt und dateien / scroipte entsprechend angepasst

--- src/database/queldaten_erzeugen.sql
+++ src/database/queldaten_erzeugen.sql
@@ -1,51 +1,42 @@
-CREATE TABLE IF NOT EXISTS quelldaten (
-    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-    Title TEXT,
-    Artist TEXT,
-    Album_Single TEXT,
-    Genre TEXT,
-    Artist_followers TEXT,
-    Explicit TEXT,
-    Album TEXT,
-    Release_date DATE,
-    Track_number INT,
-    Tracks_in_album INT,
-    duration_ms INT,
-    Genre_new TEXT ,
-    Days_since_release INT,
-    Popu_max INT,
-    Cluster TEXT
-);
+CREATE OR REPLACE FUNCTION create_quelldatei_table() RETURNS void AS $$
+BEGIN
+    DROP TABLE IF EXISTS Quelldatei;
+    CREATE TABLE Quelldatei (
+        id SERIAL PRIMARY KEY,
+        title TEXT,
+        artist TEXT,
+        album_single TEXT,
+        genre TEXT,
+        artist_followers TEXT,
+        explicit TEXT,
+        album TEXT,
+        release_day TEXT,
+        track_number TEXT,
+        tracks_in_album TEXT,
+        duration_ms TEXT,
+        genre_new TEXT,
+        days_since_release TEXT,
+        popu_max TEXT,
+        cluster TEXT
+    );
 
---Daten aus der Tabelle "Mappe2" einfügen
-INSERT INTO quelldaten (Title, Artist, Album_Single, Genre, Artist_followers, Explicit, Album, Release_date,
-                        Track_number, Tracks_in_album, duration_ms, Genre_new, Days_since_release, Popu_max, Cluster)
-SELECT
-    Title,
-    Artist,
-    Album/Single,
-    Genre,
-    Artist_followers,
-    Explicit,
-    Album,
-    Release_date,
-    Track_number,
-    Tracks_in_album,
-    duration_ms,
-    Genre_new ,
-    Days_since_release,
-    Popu_max,
-    Cluster
-FROM Mappe2;
-
---Duplikate entfernen, falls nötig
-DELETE FROM quelldaten
-WHERE rowid NOT IN (
-    SELECT MIN(rowid)
-    FROM quelldaten
-    GROUP BY Title, Artist, Album_Single, Genre, Artist_followers, Explicit, Album, Release_date,
-            Track_number, Tracks_in_album, duration_ms, Genre_new, Days_since_release, Popu_max, Cluster
-);
-
---Let´s Test it!
-SELECT * from quelldaten;
+    INSERT INTO Quelldatei (title, artist, album_single, genre, artist_followers, explicit, album, release_day,
+                            track_number, tracks_in_album, duration_ms, genre_new, days_since_release, popu_max, cluster)
+    SELECT "Title",
+           "Artist",
+           "Album/Single",
+           "Genre",
+           "Artist_followers",
+           "Explicit",
+           "Album",
+           "Release_date",
+           "Track_number",
+           "Tracks_in_album",
+           "duration_ms",
+           "Genre_new",
+           "Days_since_release",
+           "Popu_max",
+           "Cluster"
+    FROM "Mappe2";
+END;
+$$ LANGUAGE plpgsql;
