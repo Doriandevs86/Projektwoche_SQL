98228801ac21c5229d43c49559e1d3f2e0b8fb0c
Ameroras <dorian.ratzmer@data-craft-students.de>
2025-01-12 20:12:52 +0100

    .

--- src/app.py
+++ src/app.py
@@ -1,14 +1,17 @@
 import customtkinter as ctk
 from PIL import Image
-from dropdown_funktionen import get_genres_from_db
-#from dropdown_funktionen import get_interpreten_from_db
-#from dropdown_funktionen import get_rating_from_db
+from customtkinter import CTkFrame
+from sqlalchemy import column
 
+from Projektwoche_SQL.src.dropdown_funktionen import connect_to_db
+from dropdown_funktionen import get_genres_from_db
+from dropdown_funktionen import get_interpreten_from_db
+from dropdown_funktionen import get_subgenres_from_db
 
 
 ### Hauptfenster ###
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("./config/theme/custom.json")# Setze das Standardfarbschema
+ctk.set_default_color_theme("./config/theme/custom.json") # Setze das Standardfarbschema
 
 root = ctk.CTk()  # Erstelle das Hauptfenster
 root.title('Meine Musik Empfehlung')
@@ -17,42 +20,52 @@ root.resizable(width=False, height=False)
 root.configure()
 
 
-
 ### Hintergrundbild laden ###
 
 bild_path = r"assetes/image/SL.123119.26540.04.jpg"
 bild = Image.open(bild_path)
+
 # CTkImage erstellen und auf Fenstergröße skalieren
 hintergrundbild = ctk.CTkImage(bild, size=(800, 600))
+
 # CTkLabel für das Hintergrundbild erstellen
 label_hintergrund = ctk.CTkLabel(root, image=hintergrundbild, text="")
 label_hintergrund.place(relwidth=1, relheight=1)
-
 
 
 ### Buttons und Menüs ###
 
 # Genre Dropdown
 genre_ver = ctk.StringVar(value="Genre")
-genres = get_genres_from_db()
+genres = get_genres_from_db(connection=connect_to_db())
 genre_dropdown = ctk.CTkOptionMenu(root, variable=genre_ver, values=genres)
 genre_dropdown.grid(column=0, row=0, padx=20, pady=10)
 
-# # Interpreten Dropdown
-# interpret_ver = ctk.StringVar(value="Interpret")
-# interpret = get_interpreten_from_db()
-# interpret_dropdown = ctk.CTkOptionMenu(root, variable=interpret_ver, values=interpret)
-# interpret_dropdown.grid(column=0, row=1, padx=20, pady=10)
-#
-# # Rating Dropdown
+# Sub_Genre Dropdown
+sub_genre_ver = ctk.StringVar(value="Sub_Genres")
+sub_genres = get_subgenres_from_db(connection=connect_to_db())
+sub_genre_dropdown = ctk.CTkOptionMenu(root, variable=sub_genre_ver, values=sub_genres)
+sub_genre_dropdown.grid(column=0, row=1, padx=20, pady=10)
+
+# Interpreten Dropdown
+interpret_ver = ctk.StringVar(value="Interpret")
+interpret = get_interpreten_from_db(connection=connect_to_db())
+interpret_dropdown = ctk.CTkOptionMenu(root, variable=interpret_ver, values=interpret)
+interpret_dropdown.grid(column=0, row=2, padx=20, pady=10)
+
+# # Extras Dropdown
 # extras_ver = ctk.StringVar(value="Extras")
 # extras = ["ja", "Nein"]
 # extras_dropdown = ctk.CTkOptionMenu(root, variable=extras_ver,values=extras, bg_color="transparent")
-# extras_dropdown.grid(column=0, row=2, padx=20, pady=10 )
-#
-# # Start-Button
-# start_btn = ctk.CTkButton(root, text="Start",)
-# start_btn.grid(row=3, column=0, columnspan=2, padx=20, pady=10)
+# extras_dropdown.grid(column=0, row=3, padx=20, pady=10 )
+
+#ef info():
+#   infofenster = ctk.CTkTextbox(root)
+#   infofenster.grid()
+
+# Start-Button
+#tart_btn = ctk.CTkButton(root, text="Start", command=info)
+#tart_btn.grid(row=4, column=0, columnspan=2, padx=20, pady=10)
 
 
 root.mainloop()
--- src/dropdown_funktionen.py
+++ src/dropdown_funktionen.py
@@ -5,97 +5,66 @@ from sqlalchemy.orm import Session
 from sqlalchemy import Text, create_engine
 from psycopg import connect
 
-# # check_in:
-# pw = getpass('Please enter password: ')
-# connection_url = f'postgresql://postgres:{pw}@localhost:5432/musikdatenbank'
-# engine = create_engine(connection_url)
-#
-# # check connection:
-# with engine.connect() as conn_alchemy:
-#     print("SQLAlchemy connected!")
-#
-#
-# # connection psycopg:
-# pw = getpass('Please enter password: ')
-# with psycopg.connect(
-#     host='localhost',
-#     port='5432',
-#     user='postgres',
-#     password=pw,
-#     dbname='musikdatenbank',
-#     autocommit=True
-# ) as connection:
-#     print("psycopg connected!")
-
-
-def get_genres_from_db():
+# check_in:
+def connect_to_db():
     try:
-        # Verbindung zur Datenbank herstellen
-        with psycopg.connect(
+        connection = psycopg.connect(
             host='localhost',
             port='5432',
             user='postgres',
             password='Datacraft',
             dbname='musikdatenbank',
             autocommit=True
-        ) as connection:
-            with connection.cursor() as cursor:
-                cursor.execute('SELECT DISTINCT playlist_genre, playlist_subgenre FROM "Spotifydata" group by playlist_genre, playlist_subgenre;')
-                genres = cursor.fetchall()
-
-                return [genre for genre in genres]
+        )
+        print('Verbindung erfolgreich hergestellt')
+        return connection
     except Exception as e:
-        print(f"Fehler beim Abrufen der Genres: {e}")
-        return []
-get_genres_from_db()
+        print(f"Fehler beim Herstellen der Verbindung: {e}")
+        return None
 
-#def get_genres_from_db():
-#    try:
-#        # Verbindung mit SQLAlchemy zur richtigen Datenbank herstellen
-#        with engine.connect() as connection:
-#            result = connection.execute('SELECT DISTINCT "playlist_genre" FROM public."Spotifydata";')
-#            genres = [row[0] for row in result]
-#            return genres
-#    except Exception as e:
-#        print(f"Fehler beim Abrufen der Genres: {e}")
-#        return []
-#get_genres_from_db()
-#
-#def get_interpreten_from_db():
-#    conn = connect(
-#        "dbname=musikdatenbank"
-#        " user=postgres"
-#        " password=Datacraft"
-#        " host=localhost"
-#        " port=5432")
-#    cursor = conn.cursor()
-#    cursor.execute('SELECT DISTINCT track_artist FROM "Spotifydata";')
-#    artists = cursor.fetchall()
-#    cursor.close()
-#    conn.close()
-#    return [artist[0] for artist in artists]
-#print('Funktion läuft')
-#
-#def get_rating_from_db():
-#    conn = connect(
-#        "dbname=musikdatenbank"
-#        " user=postgres"
-#        " password=Datacraft"
-#        " host=localhost"
-#        " port=5432")
-#    cursor = conn.cursor()
-#    cursor.execute('SELECT DISTINCT track_popularity FROM "Spotifydata";')
-#    ratings = cursor.fetchall()
-#    cursor.close()
-#    conn.close()
-#    return [rating[0] for rating in ratings]
-#print('Funktion läuft')
-#
-#
-## Einfachere Abfrage, nur um zu prüfen, ob eine grundlegende SQL-Abfrage funktioniert
-#with engine.connect() as connection:
-#    result = connection.execute('SELECT * FROM "Spotifydata";')
-#    for row in result:
-#        print(row)
-#
-#%%
+
+
+def get_interpreten_from_db(connection):
+    try:
+        with connection.cursor() as cursor:
+            # Abfrage der einzigartigen Künstler
+            cursor.execute('''
+                SELECT DISTINCT track_artist
+                FROM "Spotifydata";
+            ''')
+            rows = cursor.fetchall()
+            return [f"{row[0]}" for row in rows]
+    except Exception as e:
+        print(f"Fehler beim Abrufen oder Verarbeiten der Künstler: {e}")
+        return []
+
+
+
+def get_genres_from_db(connection):
+    try:
+        with connection.cursor() as cursor:
+            # Abfrage der einzigartigen Künstler
+            cursor.execute('''
+                SELECT DISTINCT playlist_genre
+                FROM "Spotifydata";
+            ''')
+            rows = cursor.fetchall()
+            return [f"{row[0]}" for row in rows]
+    except Exception as e:
+        print(f"Fehler beim Abrufen oder Verarbeiten der Künstler: {e}")
+        return []
+
+def get_subgenres_from_db(connection):
+    try:
+        with connection.cursor() as cursor:
+            # Abfrage der einzigartigen Künstler
+            cursor.execute('''
+                SELECT DISTINCT playlist_subgenre
+                FROM "Spotifydata";
+            ''')
+            rows = cursor.fetchall()
+            return [f"{row[0]}" for row in rows]
+    except Exception as e:
+        print(f"Fehler beim Abrufen oder Verarbeiten der Künstler: {e}")
+        return []
+
