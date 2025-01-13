0e1ea0f70fb703988db2870a7955c2d3076bf17e
Ameroras <dorian.ratzmer@data-craft-students.de>
2025-01-13 03:51:54 +0100

    getpass problem gelöst
    .env datei angelegt und zur .gitginog hinzugefgt (dies bei gelegenheit geupdated
    
    app.py sowie dropdown.py angepasst (imports,.env laden, psycpg login angepasst, ect)

--- src/dropdown_funktionen.py
+++ src/dropdown_funktionen.py
@@ -1,19 +1,19 @@
 import psycopg
-import pandas as pd
-from getpass import getpass
-from sqlalchemy.orm import Session
-from sqlalchemy import Text, create_engine
-from psycopg import connect
+import os
+from flask.cli import load_dotenv
+
+
+load_dotenv(r'C:\Users\Admin\Documents\Projektwoche_SQL\Projektwoche_SQL\.env')
 
 # check_in:
 def connect_to_db():
     try:
         connection = psycopg.connect(
-            host='localhost',
-            port='5432',
-            user='postgres',
-            password='Datacraft',
-            dbname='musikdatenbank',
+            host=os.getenv("host") ,
+            port=os.getenv("port") ,
+            user=os.getenv("user") ,
+            password=os.getenv("password"),
+            dbname=os.getenv("dbname"),
             autocommit=True
         )
         print('Verbindung erfolgreich hergestellt')
@@ -21,22 +21,6 @@ def connect_to_db():
     except Exception as e:
         print(f"Fehler beim Herstellen der Verbindung: {e}")
         return None
-
-
-def get_interpreten_from_db(connection, genre, subgenre):
-    try:
-        with connection.cursor() as cursor:
-            # Abfrage der Interpreten basierend auf Genre und Subgenre
-            cursor.execute('''
-                SELECT DISTINCT track_artist
-                FROM "Spotifydata"
-                WHERE playlist_genre = %s AND playlist_subgenre = %s;
-            ''', (genre, subgenre))
-            rows = cursor.fetchall()
-            return [f"{row[0]}" for row in rows]
-    except Exception as e:
-        print(f"Fehler beim Abrufen oder Verarbeiten der Interpreten: {e}")
-        return []
 
 
 
@@ -54,6 +38,7 @@ def get_genres_from_db(connection):
         print(f"Fehler beim Abrufen oder Verarbeiten der Künstler: {e}")
         return []
 
+
 def get_subgenres_from_db(connection, genre):
     try:
         with connection.cursor() as cursor:
@@ -69,3 +54,18 @@ def get_subgenres_from_db(connection, genre):
         print(f"Fehler beim Abrufen oder Verarbeiten der Subgenres: {e}")
         return []
 
+
+def get_interpreten_from_db(connection, genre, subgenre):
+    try:
+        with connection.cursor() as cursor:
+            # Abfrage der Interpreten basierend auf Genre und Subgenre
+            cursor.execute('''
+                SELECT DISTINCT track_artist
+                FROM "Spotifydata"
+                WHERE playlist_genre = %s AND playlist_subgenre = %s;
+            ''', (genre, subgenre))
+            rows = cursor.fetchall()
+            return [f"{row[0]}" for row in rows]
+    except Exception as e:
+        print(f"Fehler beim Abrufen oder Verarbeiten der Interpreten: {e}")
+        return []
